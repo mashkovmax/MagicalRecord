64e6292851a5751d6f60959ae7eca82cf42774e5
Carlos Luis Seva Llor <carlos@digio.es>
2023-04-07 11:23:04 +0200

    Silencing deprecated warnings

--- MagicalRecord/Categories/NSManagedObjectContext/NSManagedObjectContext+MagicalThreading.h
+++ MagicalRecord/Categories/NSManagedObjectContext/NSManagedObjectContext+MagicalThreading.h
@@ -11,9 +11,9 @@
 
 @interface NSManagedObjectContext (MagicalThreading)
 
-+ (MR_nonnull NSManagedObjectContext *) MR_contextForCurrentThread __attribute((deprecated("This method will be removed in MagicalRecord 3.0")));
-+ (void) MR_clearNonMainThreadContextsCache __attribute((deprecated("This method will be removed in MagicalRecord 3.0")));
-+ (void) MR_resetContextForCurrentThread __attribute((deprecated("This method will be removed in MagicalRecord 3.0")));
-+ (void) MR_clearContextForCurrentThread __attribute((deprecated("This method will be removed in MagicalRecord 3.0")));
++ (MR_nonnull NSManagedObjectContext *) MR_contextForCurrentThread //__attribute((deprecated("This method will be removed in MagicalRecord 3.0")));
++ (void) MR_clearNonMainThreadContextsCache //__attribute((deprecated("This method will be removed in MagicalRecord 3.0")));
++ (void) MR_resetContextForCurrentThread //__attribute((deprecated("This method will be removed in MagicalRecord 3.0")));
++ (void) MR_clearContextForCurrentThread //__attribute((deprecated("This method will be removed in MagicalRecord 3.0")));
 
 @end
